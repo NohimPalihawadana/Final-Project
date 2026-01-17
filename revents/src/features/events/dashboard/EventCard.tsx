82e3e24ace432c78b534c432039a29840c7e5d5b
NohimPalihawadana <129199893+NohimPalihawadana@users.noreply.github.com>
2026-01-17 11:55:59 +0530

    End of Section 4.

--- revents/src/features/events/dashboard/EventCard.tsx
+++ revents/src/features/events/dashboard/EventCard.tsx
@@ -1,14 +1,17 @@
+//import { select } from "motion/react-client";
 import type { AppEvent } from "../../../lib/types";
 import EventAttendees from "./EventAttendees";
 
 type Props = {
     event: AppEvent;
+    formToggle: (event: AppEvent) => void;
+    deleteEvent?: (eventId: string) => void;
 }
 
-export default function EventCard({event}: Props) {
+export default function EventCard({ event, formToggle, deleteEvent }: Props) {
 
     const host = event.attendees.find(x => x.id === event.hostUid);
-    
+
     return (
         <div className="card card-border bg-base-100 w-full">
             <div className="card-body">
@@ -31,7 +34,10 @@ export default function EventCard({event}: Props) {
                     <div className="flex flex-1">
                         {event.description}
                     </div>
-                    <button className="btn btn-primary">View</button>
+                    <div className="flex gap-3">
+                        <button onClick={() => deleteEvent?.(event.id)} className="btn btn-error">Delete</button>
+                        <button onClick={() => formToggle(event)} className="btn btn-primary">Edit</button>
+                    </div>
                 </div>
             </div>
         </div>
--- revents/src/features/events/dashboard/EventDashboard.tsx
+++ revents/src/features/events/dashboard/EventDashboard.tsx
@@ -1,14 +1,39 @@
-import { AnimatePresence, motion } from "motion/react";
+import { AnimatePresence, motion } from "framer-motion";
 import { events } from "../../../lib/data/sampleData";
 import EventForm from "../form/EventForm";
 import EventCard from "./EventCard";
+import type { AppEvent } from "../../../lib/types";
+import { useEffect, useState } from "react";
 
 type Props = {
   formOpen: boolean;
   setFormOpen: (isOpen: boolean) => void;
-}
+  formToggle: (event: AppEvent | null) => void;
+  selectedEvent: AppEvent | null;
+};
 
-export default function EventDashboard({ formOpen, setFormOpen }: Props) {
+export default function EventDashboard({ formOpen, setFormOpen, formToggle, selectedEvent }: Props) {
+  const [appEvents, setAppEvents] = useState<AppEvent[]>([]);
+
+
+  const handleCreateEvent = (event: AppEvent) => {
+    setAppEvents(prev => [...prev, event]);
+  };
+
+  const handleUpdateEvent = (updatedEvent: AppEvent) => {
+    setAppEvents(prevState => {
+      return prevState.map(event => event.id === updatedEvent.id ? updatedEvent : event);
+    });
+  }
+
+  const handleDeleteEvent = (eventId: string) => {
+    setAppEvents(prevState => prevState.filter(event => event.id !== eventId));
+  };
+
+  useEffect(() => {
+    setAppEvents(events);
+  }, []);
+
   return (
     <div className="flex flex-row w-full gap-6">
       <div className="w-3/5">
@@ -17,35 +42,41 @@ export default function EventDashboard({ formOpen, setFormOpen }: Props) {
             initial={{ opacity: 0, x: -200 }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -200 }}
-            transition={{ duration: 0.3, ease: 'easeInOut' }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
           >
             <div className="flex flex-col gap-4">
-              {events.map((event) => (
-                <EventCard key={event.id} event={event} />
+              {appEvents.map(event => (
+                <EventCard
+                  formToggle={formToggle}
+                  deleteEvent={handleDeleteEvent}
+                  key={event.id}
+                  event={event} />
               ))}
             </div>
-
-
           </motion.div>
         </AnimatePresence>
-
       </div>
 
-      <div className="w-2/5">
-
+      <div className="w-2/5 overflow-hidden">
         <AnimatePresence>
           {formOpen && (
             <motion.div
               initial={{ opacity: 0, x: 200 }}
               animate={{ opacity: 1, x: 0 }}
               exit={{ opacity: 0, x: 200 }}
-              transition={{ duration: 0.3, ease: 'easeInOut' }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
             >
-              <EventForm setFormOpen={setFormOpen} />
+              <EventForm
+                key={selectedEvent ? selectedEvent.id : 'new'}
+                setFormOpen={setFormOpen}
+                createEvent={handleCreateEvent}
+                selectedEvent={selectedEvent ?? undefined}
+                updateEvent={handleUpdateEvent}
+              />
             </motion.div>
           )}
         </AnimatePresence>
       </div>
     </div>
-  )
+  );
 }
